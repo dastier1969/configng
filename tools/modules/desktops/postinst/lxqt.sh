c3b20f259414ac8ae94de06ef7b61f8ce22fc1f1
RichN <r.e.neese1969@gmail.com>
2026-05-09 08:25:31 -0400

    start lxqt

---
+++ tools/modules/desktops/postinst/lxqt.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set +e
+
+# Configure SDDM theme and wallpaper
+# plasma-chili only works with X11 greeter (Ubuntu), skip on Wayland greeter (Trixie)
+if [ -d /usr/share/sddm/themes/plasma-chili ] && [ -f /etc/sddm.conf ]; then
+	# Ubuntu: has /etc/sddm.conf, uses X11 greeter
+	cp /usr/share/backgrounds/armbian/armbian03-Dre0x-Minum-dark-3840x2160.jpg \
+		/usr/share/sddm/themes/plasma-chili/components/artwork/background.jpg 2>/dev/null || true
+fi
+
+
